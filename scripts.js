f116af7229911c4c5df731fb356e15a63607848f
Mateusz Zylka <mateuszzylkaa@gmail.com>
2018-02-11 14:55:30 +0100

    Add task_9_4

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function drawTree(height) {
+	for (var i = 0; i < height; i++) {
+		var star = '';
+		for (var j = height; j > i + 1; j--) {
+			star += ' ';
+		}
+		for (var y = 0; y <= i * 2; y++) {
+			star += '*';
+		}
+		console.log(star);
+	}
+};
+
+console.log(drawTree(10));
